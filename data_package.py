d93981c428e4eae9396c635754b97b3177774ab9
Brayan Jaraba <brayanjaraba2006@gmail.com>
2025-05-26 18:28:00 -0500

    Tenemos hasta el reservoir

--- data_package.py
+++ data_package.py
@@ -2,11 +2,27 @@ import secrets
 
 
 class DataPackage:
-    def __init__(self, source_ip, destionation_ip, payload_size, flagged):
-        self.source_ip = source_ip
-        self.destionation_ip = destionation_ip
-        self.payload_size = payload_size
-        self.flagged = flagged
+    def __init__(
+        self, source_ip: str, destionation_ip: str, payload_size: int, flagged: bool
+    ):
+        self.source_ip: str = source_ip
+        self.destionation_ip: str = destionation_ip
+        self.payload_size: int = payload_size
+        self.flagged: bool = flagged
+
+    def get_hash(self, size: int = 10000000) -> int:
+        hash_count: int = 0
+        for i, char in enumerate(self._get_str()):
+            hash_count += ord(char) * i
+        return hash_count % size
+
+    def _get_str(self) -> str:
+        return (
+            self.destionation_ip
+            + self.source_ip
+            + self.payload_size.__str__()
+            + self.flagged.__str__()
+        )
 
 
 def data_package_generator() -> DataPackage:
@@ -19,3 +35,7 @@ def data_package_generator() -> DataPackage:
 
 def ip_generator() -> str:
     return ".".join([str(secrets.randbelow(256)) for _ in range(4)])
+
+
+if __name__ == "__main__":
+    print(data_package_generator().get_hash())
